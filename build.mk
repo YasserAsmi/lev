6600ec53a270c8edd0f3d7e785645a707faf4fe3
Yasser Asmi <yasserasmi@live.com>
2016-02-14 19:18:37 -0800

    Update build.mk

--- build.mk
+++ build.mk
@@ -2,7 +2,7 @@
 # Released under the MIT License (http://opensource.org/licenses/MIT)
 #
 # build.mk:
-# 	CONFIG    Build config (debug, release)
+# 	CONFIG    Build config (debug, opttest, release)
 # 	TYPE      Type of project (lib, exe)
 # 	SOURCES   Names of cpp files; ex: str.cpp util.cpp
 # 	INCLUDES  Include directories with -I compiler option; ex: -I../include -I/usr/local/include
@@ -17,7 +17,7 @@
 #
 #   make          -- builds
 #   make -B       -- builds even if up to date
-#   make cleanall -- cleans
+#   make cleanall -- cleans all DIRS
 #   make clean    -- cleans current folder/makefile only
 #   make xx._s    -- generates assembly/listing file for xx.cpp
 #   make xx._p    -- generates preprocessed file for xx.cpp
@@ -26,13 +26,18 @@ CONFIG ?= debug
 TYPE ?= exe
 INCLUDES ?= -I/usr/local/include
 
-OBJS = $(SOURCES:.cpp=.o)
+OBJS = $(patsubst %.cpp,oo/%.o,$(SOURCES))
+
 PWD = $(shell pwd)
 
 ifeq ($(CONFIG)	,debug)
 CXXFLAGS += -g -Wall
 else
+ifeq ($(CONFIG)	,opttest)
+CXXFLAGS += -g -O3 -Wall
+else
 CXXFLAGS += -O3 -Wall -DNDEBUG
+endif
 endif
 
 all: dirs extmakes cfgcheck mkdirs $(OUT)
@@ -42,14 +47,16 @@ all: dirs extmakes cfgcheck mkdirs $(OUT)
 cfgcheck:
 ifneq ($(CONFIG)	,release)
 ifneq ($(CONFIG)	,debug)
-	@echo "Error: Invalid CONFIG 	'$(CONFIG)	'' (options: debug,release)"
+ifneq ($(CONFIG)	,opttest)
+	@echo "Error: Invalid CONFIG '$(CONFIG)	'' (options: debug, opttest, release)"
 	@exit 1
+endif
 endif
 endif
 	@echo "Making '$(PWD)' CONFIG="$(CONFIG)
 
 $(OUT): $(OBJS) $(LIBS)
-	echo $(LIBS)
+	@echo $(LIBS)
 ifeq ($(TYPE),lib)
 	$(AR) rcs $(OUT) $(OBJS)
 endif
@@ -57,20 +64,26 @@ ifeq ($(TYPE),exe)
 	$(CXX) -o $@ $^ ${LDFLAGS} $(LIBS) $(INSLIBS)
 endif
 
--include $(OBJS:.o=._d)
+-include $(OBJS:.o=.i)
 
-%.o: %.cpp
-	$(CXX) -c $(INCLUDES) $(CXXFLAGS) $(PWD)/$*.cpp -o $*.o
-	$(CXX) -MM $(INCLUDES) $(CXXFLAGS) $*.cpp > $*._d
-	@cp -f $*._d $*._d.tmp
-	@sed -e 's/.*://' -e 's/\\$$//' < $*._d.tmp | fmt -1 | sed -e 's/^ *//' -e 's/$$/:/' >> $*._d
-	@rm -f $*._d.tmp
+oo/%.o: %.cpp
+	@mkdir -p oo
+	$(CXX) -c $(INCLUDES) $(CXXFLAGS) $(PWD)/$*.cpp -o oo/$*.o
+	@$(CXX) -MM $(INCLUDES) $(CXXFLAGS) $*.cpp > oo/$*.i.tmp
+	@echo -n "oo/" > oo/$*.i
+	@cat oo/$*.i.tmp >> oo/$*.i
+	@sed -e 's/.*://' -e 's/\\$$//' < oo/$*.i.tmp | fmt -1 | sed -e 's/^ *//' -e 's/$$/:/' >> oo/$*.i
+	@rm -f oo/$*.i.tmp
 
 %._p: %.cpp
-	$(CXX) -E -P $(INCLUDES) $(CXXFLAGS) $(PWD)/$*.cpp -o $*._p
+	@mkdir -p oo
+	$(CXX) -E -P $(INCLUDES) $(CXXFLAGS) $(PWD)/$*.cpp -o oo/$*._p
+	@echo "Created preprocessor output: oo/"$*._s
 
 %._s: %.cpp
-	$(CXX) -c -Wa,-adhln -g $(INCLUDES) $(CXXFLAGS) $*.cpp > $*._s
+	@mkdir -p oo
+	$(CXX) -c -Wa,-adhln -g $(INCLUDES) $(CXXFLAGS) $*.cpp > oo/$*._s
+	@echo "Created assembly output: oo/"$*._s
 
 dirs: $(DIRS)
 
@@ -79,11 +92,13 @@ extmakes: $(EXTMAKES)
 mkdirs: $(MKDIRS)
 
 clean:
-	rm -f $(OUT) *.o *._d *._p *._s
+	rm -f $(OUT) oo/*.o oo/*.i oo/*._p oo/*._s
+	rmdir oo
 
 cleanall: TARG:=cleanall
 cleanall: $(DIRS) $(EXTMAKES)
-	rm -f $(OUT) *.o *._d *._p *._s
+	rm -f $(OUT) oo/*.o oo/*.i oo/*._p oo/*._s
+	rmdir oo
 
 $(DIRS):
 	@$(MAKE) -C $@ $(TARG)
